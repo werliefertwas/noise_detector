852719fdf1b58804f92373acd01a1c532f360dcc
Nils Löwe <nils@loewe.io>
2016-05-10 11:34:12 +0200

    add light sensor to led matrix

---
+++ sensor_matrix/sensor_matrix.ino
@@ -0,0 +1,53 @@
+#include <MAX7219_8x8_matrix.h>
+
+//    MAX7219_8x8_matrix(DIN, CS, CLK);
+MAX7219_8x8_matrix LEDmatrix(2,3,4);
+int i = 0;
+int delay_time = 20;  //ms
+int m[8] = {1,1,1,1,1,1,1,1};
+int p = 0;
+const int sensorPin = 0;
+const int offset = 120;
+int level;
+
+int set_leds(int input) {
+  int ret = 0;
+  for (int i=0; i<8; i++) {
+    if (i < input) { ret |= (0x01 << i); }
+  }
+  return ret;
+}
+
+void output_matrix(void) {
+  for (int col=0;col<8;col++){
+   LEDmatrix.set_col(col,set_leds(m[col]));
+   delay(delay_time);
+  }
+}
+
+void matrix_push(int val) {
+  for (int i=7; i>0; i--) {
+    m[i] = m[i-1];
+  }
+  m[0] = val;
+}
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  LEDmatrix.clear();
+  delay(1000);
+  LEDmatrix.setBrightness(8);
+  randomSeed(6);
+  Serial.begin(9600);
+}
+
+void loop() {
+  level = analogRead(sensorPin) - offset;
+  Serial.println(level);
+  if (level > 0) { matrix_push(level/70); }
+  else { matrix_push(0); }
+
+  output_matrix();
+
+  delay(100);
+}
